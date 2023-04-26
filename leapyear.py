bdd1a7da801df1f15b22503fc1a7f2c130bbce48
reshmarb8547 <reshmarb8547>
2023-04-26 13:56:53 +0530

    to check whether it is leap year or not

--- leapyear.py
+++ leapyear.py
@@ -1,11 +1,6 @@
 year = int(input(" a year: "))
-if (year%4==0):
-    print('its a leap year')
-
-elif (year%100==0):
-    print('its a leap year')
-elif(year%400==0):
+if (year%4==0 and year%100==0 and year%400==0):
     print('its a leap year')
 
 else:
-    print('its not aleap year')
+    print('its not a leap year')
--- patterns.py
+++ patterns.py
@@ -1,6 +1,32 @@
+# #
+# for i in range(1,5):
+#     for j in range(i,5):
+#         print(i,end="")
 #
-for i in range(1,5):
-    for j in range(i,5):
-        print(i,end="")
+#     print()
 
+# for i in range(1,7):
+#     for j in range(1,7):
+#         print('*',end =" ")
+#     print()
+
+for i in range(1, 7):
+    for j in range(1, 7):
+        if j==1 or i==6 or i==j :
+            print('*',end = " ")
+        else:
+            print(" ",end=" ")
     print()
+
+
+
+
+
+
+
+
+
+
+
+
+
